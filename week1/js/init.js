73e191a15ecd7f62de93ff91fa2c0dc45d2ae016
Catherine Hu <64565524+catherinehhu@users.noreply.github.com>
2023-04-09 17:25:07 -0700

    week 1 assignment

--- week1/js/init.js
+++ week1/js/init.js
@@ -1,15 +1,17 @@
 // JavaScript const variable declaration
-const map = L.map('the_map').setView([34.0709, -118.444], 15); // (1)!
+const map = L.map('the_map').setView([35,-116], 7); // (1)!
 
 // Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map); // (2)!
 
 //JavaScript let variable declaration to create a marker
-let marker = L.marker([34.0709, -118.444]).addTo(map) // (3)!
-		.bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I used to work!')
-		.openPopup();
-let marker2 = L.marker([34.0719, -118.444]).addTo(map) // (3)!
-.bindPopup('this is bruinwalk!')
-.openPopup();
+let deathvalley = L.marker([36.5054, -117.0794]).addTo(map) // (3)!
+        .bindPopup('One of the first parks I visited in California'); 
+
+let joshuatree = L.marker([33.8734, -115.9010]).addTo(map) // (3)!
+.bindPopup('One of my favorite national parks for stargazing'); 
+
+let anzaborrego = L.marker([33.1005, -116.3013]).addTo(map) // (3)!
+.bindPopup('So many flowers in the spring'); 
