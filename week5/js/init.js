8b65fcd06c81055f1ee85ce7fa16c83c6c00eeff
Catherine Hu <64565524+catherinehhu@users.noreply.github.com>
2023-05-16 09:34:07 -0700

    week 5 lab 1

---
+++ week5/js/init.js
@@ -0,0 +1,36 @@
+// declare variables
+let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
+
+// use the variables
+const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// create a function to add markers
+function addMarker(lat,lng,title,message){
+    console.log(message)
+    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
+    return message
+}
+
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_U52VzKmPa1zCXEOA8nIFRmn5dn3Pee2JkhsaMGlivuWmFWZdA_lOj3MVSeGkBg_vvVu_ivX6YeVy/pub?output=csv"
+
+function loadData(url){
+    Papa.parse(url, {
+        header: true,
+        download: true,
+        complete: results => processData(results)
+    })
+}
+
+function processData(results){
+    console.log(results)
+    results.data.forEach(data => {
+        console.log(data)
+        addMarker(data['lat'],data['lng'], data["What is the highest level of education you've completed?"], data['What is your age? '])
+    })
+}
+
+loadData(dataUrl)
