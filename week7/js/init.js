09d0104405d1b1e8130f43c3a4f637d44546bdee
Catherine Hu <64565524+catherinehhu@users.noreply.github.com>
2023-06-06 14:32:12 -0700

    lab10

--- week7/js/init.js
+++ week7/js/init.js
@@ -18,7 +18,7 @@ let circleOptions = {
     fillOpacity: 0.8
 }
 
-const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_U52VzKmPa1zCXEOA8nIFRmn5dn3Pee2JkhsaMGlivuWmFWZdA_lOj3MVSeGkBg_vvVu_ivX6YeVy/pub?output=csv"
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSmc1uUHcf8Fjo_ylciM3OEWxR4Y9MnwixM9U3-BDFGm_e4Aks-LAxEc8DwyT_X_Yyub7DQwIDBcwHw/pub?gid=1644397384&single=true&output=csv"
 
 // define the leaflet map
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
@@ -34,31 +34,14 @@ let Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/r
 Esri_WorldGrayCanvas.addTo(map);
 
 function addMarker(data){
-    if(data["What is the highest level of education you've completed?"] == "No schooling completed"){
-        circleOptions.fillColor = "red"
-        vaccinated.addLayer(L.circleMarker([data.lat,data.lng],circleOptions).bindPopup(`<h2>${data["What is the highest level of education you've completed?"]}</h2>`))
-        createButtons(data.lat,data.lng,data["What is the highest level of education you've completed?"])
-        }
-    else{
+    
         circleOptions.fillColor = "blue"
-        nonVaccinated.addLayer(L.circleMarker([data.lat,data.lng],circleOptions).bindPopup(`<h2>${data["What is the highest level of education you've completed?"]}</h2>`))
-        createButtons(data.lat,data.lng,data["What is the highest level of education you've completed?"])
-    }
+        // createButtons(data.lat,data.lng)
+        console.log("yay")
     return data
 }
 
-function createButtons(lat,lng,title){
-    const newButton = document.createElement("button"); // adds a new button
-    newButton.id = "button"+title; // gives the button a unique id
-    newButton.innerHTML = title; // gives the button a title
-    newButton.setAttribute("lat",lat); // sets the latitude 
-    newButton.setAttribute("lng",lng); // sets the longitude 
-    newButton.addEventListener('click', function(){
-        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
-    })
-    const spaceForButtons = document.getElementById('placeForButtons')
-    spaceForButtons.appendChild(newButton);//this adds the button to our page.
-}
+
 
 function loadData(url){
     Papa.parse(url, {
@@ -74,8 +57,7 @@ function processData(results){
         console.log(data)
         addMarker(data)
     })
-    vaccinated.addTo(map) // add our layers after markers have been made
-    nonVaccinated.addTo(map) // add our layers after markers have been made  
+
     // let allLayers = L.featureGroup([vaccinated,nonVaccinated]);
     // map.fitBounds(allLayers.getBounds());
 }
